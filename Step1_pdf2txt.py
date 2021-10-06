0342c7957134bb9d6c764295caafe6b369a5a7ee
ChrisCraddock <30630937+ChrisCraddock@users.noreply.github.com>
2021-10-06 04:09:40 -0400

    Update Step1_pdf2txt.py

--- Step1_pdf2txt.py
+++ Step1_pdf2txt.py
@@ -3,8 +3,9 @@ import os
 import time
 
 #Scan Directory
-
-directory = 'C:\\Users\\cmc03\\OneDrive\\Desktop\\python'
+#Assign folder to  variable 'directory'
+#Open the variable 'directory' and make a copy of all PDF files as a TXT file.
+directory = 'C:\\path\\to\\folder'
 for file in os.listdir(directory):
     if not file.endswith(".pdf"): #Skip non-PDF files
         continue
@@ -13,7 +14,7 @@ for file in os.listdir(directory):
         pageObj = pdfReader.getPage(0)
         text=pageObj.extractText()
         print("Writing File "+ file +" to TXT.")
-        file2=open(r"C:\Users\cmc03\OneDrive\Desktop\python\\"+file.replace('pdf','txt'),"a") #Write as TXT files
+        file2=open(r"C:\path\to\folder\\"+file.replace('pdf','txt'),"a") #Write as TXT files.  Needs the double \\ at the end
         file2.writelines(text)
         print("Done saving.")
         file2.close()
